eda8abc19d312470c16a2fbd8df67c9d7beaaf6f
ArcidesFerrao <cidesferrao@gmail.com>
2025-05-06 22:04:31 +0200

    contact form test and redesigned

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 import Providers from "./providers";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
-import PromoSection from "@/components/PromoSection";
+// import PromoSection from "@/components/PromoSection";
 // import { Breadcrumbs } from "@/components/Breadcrumbs";
 
 export const metadata: Metadata = {
@@ -30,7 +30,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${ubuntu.className} antialiased`}>
         <Providers session={session}>
-          <PromoSection />
+          {/* <PromoSection /> */}
           <NavBar />
           {/* <Breadcrumbs withHome> */}
           <Toaster position="top-center" />
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,48 +1,101 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function ContactForm() {
+  const [isSending, setIsSending] = useState(false);
+  // const [showToast, setShowToast] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSending(true);
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch("https://formspree.io/f/xvgaadaz", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (response.ok) {
+        form.reset();
+        toast.success("Mensagem enviada com sucesso!");
+      } else {
+        toast.error("Erro ao enviar. Tente novamente.");
+      }
+    } catch (error) {
+      toast.error("Erro ao enviar. Tente novamente.");
+      console.error("Erro ao enviar: ", error);
+    } finally {
+      setIsSending(false);
+    }
+  };
+
   return (
     <form
-      action="https://formspree.io/f/xvgaadaz"
-      method="POST"
-      className="contact-form flex flex-col w-4/6 gap-4"
+      onSubmit={handleSubmit}
+      className="contact-form flex flex-col w-4/6 gap-8"
     >
-      <div className="full-name flex flex-col gap-4">
+      <div className="full-name flex  gap-4">
+        <div className="flex justify-between w-full">
+          {/* <label htmlFor="name">Name</label> */}
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nome"
+            required
+          />
+        </div>
+        <div className="flex justify-between w-full">
+          {/* <label htmlFor="apelido">Apelido</label> */}
+          <input
+            type="text"
+            name="apelido"
+            id="apelido"
+            placeholder="Apelido"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex justify-between ">
+        {/* <label htmlFor="email">Email</label> */}
         <input
-          type="hidden"
-          name="_text"
-          id=""
-          value="https://vesteousadia.com/contacto/obrigado"
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+          required
         />
-        <div className="flex justify-between ">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" required />
-        </div>
-        <div className="flex justify-between ">
-          <label htmlFor="apelido">Apelido</label>
-          <input type="text" name="apelido" id="apelido" required />
-        </div>
       </div>
       <div className="flex justify-between ">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" required />
+        {/* <label htmlFor="assunto">Assunto</label> */}
+        <input type="text" name="assunto" id="assunto" placeholder="Assunto" />
       </div>
-      <div className="flex justify-between ">
-        <label htmlFor="assunto">Assunto</label>
-        <input type="text" name="assunto" id="assunto" />
-      </div>
-      <div className="flex justify-between ">
+      {/* <div className="flex justify-between ">
         <label htmlFor="mensagem">Mensagem</label>
-      </div>
+      </div> */}
       <div className="">
         <textarea
-          className="flex w-full h-16 rounded-sm"
+          className="flex w-full rounded-sm"
           name="mensagem"
           id="mensagem"
+          placeholder="Em que podemos ajudar?"
           required
         ></textarea>
       </div>
-      <input type="submit" value="Enviar Mensagem" id="enviar" />
+      <input
+        type="submit"
+        value={isSending ? "Enviando..." : "Enviar Mensagem"}
+        className="my-4"
+        id="enviar"
+      />
     </form>
   );
 }
